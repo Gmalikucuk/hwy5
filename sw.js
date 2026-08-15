03136fc2bb21a616ef9974f5ac7b293f1b247bf8
Mehmet Kucuk <gmalikucuk@gmail.com>
2026-08-14 17:53:24 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * and phones will happily run last week's app for ever, which is the classic
  * way this goes wrong.
  */
-var VERSION = 'hwy5-2026-08-13-p';
+var VERSION = 'hwy5-2026-08-14-a';
 var SHELL = ['/', '/index.html', '/manifest.json', '/icon180.png', '/icon512.png'];
 
 self.addEventListener('install', function (e) {
